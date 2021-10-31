e7e519e439b3561b2bb87f709724f8bfd33be1f4
justincdavis <davis.justin@mssm.org>
2021-10-31 16:02:26 -0600

    added camera feed and cascade

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import cv2
 import numpy as np
+import sys
 
 def main():
 
+    face_detector = cv2.CascadeClassifier("data/haarcascade_frontalfacce_defualt.xml")
+    camera = cv2.VideoCapture(0)
+
+    while True:
+        got_image, bgr_image = camera.read()
+        if not got_image:
+            sys.exit()
+
+
+        cv2.imshow("camera feed", bgr_image)
+        cv2.waitKey(0)
 
     print("all done")
 
