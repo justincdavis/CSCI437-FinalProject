8c055995cf62298ddb8d17d3f95b117ce5d93cbc
nneff <91347227+nneff@users.noreply.github.com>
2021-11-14 16:32:45 -0700

    Face_cropped_image

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
-import sys
+import sys, os
 from PIL import Image
 from animation import Character, init_Character
 
 def main():
 
-    face_detector = cv2.CascadeClassifier("data/haarcascade_frontalfacce_default.xml")
+    path = os.path.join(sys.path[0], "data/haarcascade_frontalface_default.xml")
+    face_detector = cv2.CascadeClassifier(path)
     eye_cascade = cv2.CascadeClassifier("data/haarcascade_eye.xml")
     camera = cv2.VideoCapture(0)
 
@@ -21,9 +22,24 @@ def main():
             sys.exit()
 
         gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
+        faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
+        print(faces, "\n")
+        if faces == ():
+            faces = old_faces
+
+        x_face = faces[0][0]
+        y_face = faces[0][1]
+        face_width = faces[0][2]
+        face_height = faces[0][3]
+
+        #face_image = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
+        #face_image = cv2.rectangle(face_image, (x_face, y_face), (x_face+face_width, y_face+face_height), (0,0,255), 5)
+        face_image = gray_image[int(y_face):int(y_face+face_height), int(x_face):int(x_face+face_width)]
+
+        
 
         cv2.imshow("camera feed", bgr_image)
+        cv2.imshow("test", face_image)
         key_pressed = cv2.waitKey(10) & 0xFF
         if key_pressed == 27:
             break  # Quit on ESC
@@ -42,6 +58,7 @@ def main():
         opencv_image=cv2.cvtColor(numpy_image, cv2.COLOR_BGRA2RGBA) 
         cv2.imwrite('output.png', opencv_image) 
         cv2.imshow('output.png', opencv_image)
+        old_faces = faces
         key_pressed = cv2.waitKey(10) & 0xFF
         if key_pressed == 27:
             break  # Quit on ESC
