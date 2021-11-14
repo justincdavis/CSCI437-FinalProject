d761fdb8e08322cf37d48f60783f9d26763cfbf2
nneff <91347227+nneff@users.noreply.github.com>
2021-11-14 16:00:09 -0700

    first changes

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 def main():
 
     face_detector = cv2.CascadeClassifier("data/haarcascade_frontalfacce_default.xml")
+    eye_cascade = cv2.CascadeClassifier("data/haarcascade_eye.xml")
     camera = cv2.VideoCapture(0)
 
     while True:
@@ -12,6 +13,8 @@ def main():
         if not got_image:
             sys.exit()
 
+        gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
 
         cv2.imshow("camera feed", bgr_image)
         key_pressed = cv2.waitKey(10) & 0xFF
