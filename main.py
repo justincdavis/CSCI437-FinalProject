146bbf279788d026f7016ba2dfa86e8ccf0a512c
nneff <91347227+nneff@users.noreply.github.com>
2021-11-21 17:05:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ from animation import Character, init_Character, generate_frame
 # If draw is false or not given, returns the eyes given by the cascade and the input image
 # If draw is true then the image has the eye bounding boxes drawn on it
 def detectEyes(image, cascade, draw=False):
-    gray_image = cv2.cvtColor(image, cv2.BGR2GRAY)
-    eyes = cascade.detectMultiScale(gray_image)
+    #gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    eyes = cascade.detectMultiScale(image)
     if(draw):
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(image,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
     return eyes, image
 
-def crop2Face(image, cascade, last_face):
+def crop2Face(image, cascade):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = cascade.detectMultiScale(gray_image)
     if faces is not ():
@@ -25,6 +25,29 @@ def crop2Face(image, cascade, last_face):
         h = faces[0][3]
         face_image = gray_image[y:y+h, x:x+w]
         return face_image, faces
+    else:
+        return None, None
+
+def crop2Eyes(image, eyes):
+    height, width = image.shape
+    if eyes is not ():
+        left_eye, right_eye = np.zeros_like(eyes[0]), np.zeros_like(eyes[0])
+        i = 0
+        for eye in eyes:
+            x = eye[0]
+            y = eye[1]
+            w = eye[2]
+            h = eye[3]
+            if i < 2:
+                if x > width/2:
+                    right_eye = image[y:y + h, x:x + w]
+                if x <= width/2:
+                    left_eye = image[y:y + h, x:x + w]
+            i += 1
+        if np.sum(right_eye) == 0 or np.sum(left_eye) == 0:
+            right_eye, left_eye = image, image
+            print("Error detecting eyes. \n")
+        return left_eye, right_eye
     else:
         return None, None
 
@@ -38,6 +61,7 @@ def resizeFaceImage(face_image, scale_percent):
 
 def main():
 
+    #Initialize paths for haarcascade detection
     face_path = os.path.join(sys.path[0], "data/haarcascade_frontalface_default.xml")
     face_detector = cv2.CascadeClassifier(face_path)
     eye_path = os.path.join(sys.path[0], "data/haarcascade_eye.xml")
@@ -56,35 +80,27 @@ def main():
         got_image, bgr_image = camera.read()
         if not got_image:
             sys.exit()
-
-        face_image, face = crop2Face(bgr_image, face_detector, last_face)
+        #image_height, image_width, _ = bgr_image.shape
+        #fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+        #videoWriter = cv2.VideoWriter("output.avi", fourcc=fourcc, fps=20.0,
+                                        #frameSize=(image_width, image_height))
+        face_image, face = crop2Face(bgr_image, face_detector)
         if face_image is not None and face is not None:
-            cv2.imshow("test", face_image)
-            cv2.imshow("camera feed", bgr_image)
-            last_face = face
+            #videoWriter.write(bgr_image)
+            eyes, eye_image = detectEyes(face_image, eye_detector, draw=True)
+            right_eye, left_eye = crop2Eyes(eye_image, eyes)
+            if right_eye is not None and left_eye is not None:
+                image = generate_frame(c, scale, attributes, images)
+
+                cv2.imshow("output.png", image)
+                cv2.imshow("camera feed", bgr_image)
+                cv2.imshow("testR", right_eye)
+                cv2.imshow("testL", left_eye)
+
             key_pressed = cv2.waitKey(10) & 0xFF
             if key_pressed == 27:
                 break  # Quit on ESC
-
-            # identify faces
-
-            # fit bounding boxes
-
-            # choose sprties
-            # edit the attrributes list
-
-            image = generate_frame(c, scale, attributes, images)
-            cv2.imshow("output.png", image)
-            key_pressed = cv2.waitKey(10) & 0xFF
-            if key_pressed == 27:
-                break  # Quit on ESC
-
-
-
-    #after exiting while loop, read images in array and convert to video
 
     print("all done")
-
-
 if __name__ == "__main__":
     main() 
